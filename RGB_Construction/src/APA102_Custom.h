d88964950577b4b980c1f955f43ec5f30ac51ecf
Zachary Rivernider <z.rivernider@gmail.com>
2022-10-02 11:09:08 -0400

    Refactored code to be clear and easier to read

--- RGB_Construction/src/APA102_Custom.h
+++ RGB_Construction/src/APA102_Custom.h
@@ -1,10 +1,9 @@
-#ifndef APA102_Cust_h
-#define APA102_Cust_hS
+#pragma once
 
 #include <SPI.h>
 // arduino::MbedSPI SPI1(NC, 3, 2);
 
-class APA102_Cust
+class APA102
 {
 private:
   
@@ -16,99 +15,25 @@ private:
   pixel *pixels;
 
 public:
-  APA102_Cust(uint16_t count) {
+  APA102(uint16_t count) {
     length = count;
     pixels = new pixel[count];
   } 
-  ~APA102_Cust() {
+  ~APA102() {
     delete[] pixels;
   }
-  unsigned int length = 0;
-  
-  void Init(void)
+  int length = 0;
+  void Init(void);
+  int CountPixels(void) {return length;}
+  pixel MakeColor(uint8_t red, uint8_t green, uint8_t blue);
+  void Show(void);
+  void showUniform(uint8_t red, uint8_t green, uint8_t blue);
+  pixel& operator [] (int i)
   {
-    SPI.begin();
-    for(uint16_t i = 0; i < length; i++) pixels[i] = MakeColor(0, 0, 0);
-    Show(); //should clear all
+    return pixels[i];
   }
-  uint16_t CountPixels(void) {return length;}
-
-  pixel MakeColor(uint8_t red, uint8_t green, uint8_t blue) {
-    pixel out;
-    out.r = red;
-    out.g = green;
-    out.b = blue;
-    return out;
-  }
-  void Show(void)
-  {
-    SPI.beginTransaction(SPISettings(8000000ul, MSBFIRST, SPI_MODE0));
-
-    //32 bits of zeros to init xfer
-    SPI.transfer(0);
-    SPI.transfer(0);
-    SPI.transfer(0);
-    SPI.transfer(0);
-
-    for(uint16_t i = 0; i < length; i++)
-    {
-        SPI.transfer(0xFF);
-        SPI.transfer(pixels[i].b);
-        SPI.transfer(pixels[i].g);
-        SPI.transfer(pixels[i].r);
-    
-    
-    }
-    
-    SPI.endTransaction();
-  }
-  void showUniform(uint8_t red, uint8_t green, uint8_t blue)
-  {
-    SPI.beginTransaction(SPISettings(8000000ul, MSBFIRST, SPI_MODE0));
-
-    //32 bits of zeros to init xfer
-    SPI.transfer(0);
-    SPI.transfer(0);
-    SPI.transfer(0);
-    SPI.transfer(0);
-
-    for(uint16_t i = 0; i < length; i++)
-    {
-
-      SPI.transfer(0xFF);
-      SPI.transfer(blue);
-      SPI.transfer(green);
-      SPI.transfer(red);
-
-    }
-    for(int i = 0; i < length / 20 +4; i++)
-    {
-        SPI.transfer(0);
-    }
-    // SPI.transfer(0xFF);
-    // SPI.transfer(0xFF);
-    // SPI.transfer(0xFF);
-    // SPI.transfer(0xFF);
-
-    SPI.endTransaction();
-  }
-
-
-//
-//  uint32_t SetPixel(uint16_t i, uint32_t color)
-//  {
-//    return pixels[i] = color;
-//  }
-
-//   uint32_t& operator [] (int i)
-//   {
-//     return pixels[i];
-//   }
-// };
 };
   
 
 
 
-
-#endif
--- RGB_Construction/src/main.cpp
+++ RGB_Construction/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <APA102_Custom.h>
-APA102_Cust ledStrip(150);
+APA102 ledStrip(60);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
