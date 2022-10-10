4adf2022e3655d07ee04d49c7aa7f98592122a2d
Zachary Rivernider <z.rivernider@gmail.com>
2022-10-10 19:12:46 -0400

    Working Primary and secondary I2C communication method

--- I2C_Pico_Test/src/main.cpp
+++ I2C_Pico_Test/src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include<Wire.h>
 void writeReg(uint32_t addr, uint8_t numbBits, uint8_t offset, uint32_t data);
+void writeI2CInt(int data, byte addr);
+int readI2CInt(byte reg);
 uint32_t I2C_Con = I2C0_BASE | 0x00;
 uint32_t I2C_En = I2C0_BASE | 0x6c;
 void setup() {
   // put your setup code here, to run once:
   // I2C0_BASE & I2C_IC_CON_BITS &= !0x6;
-  
+  Serial.begin(9600);
   Wire.begin();
   writeReg(I2C_En, 1, 0, 0x0); // Disable I2C
   // writeReg(I2C_Con, 2, 1, 0x1); // Turn on fast mode, but it is not
@@ -25,48 +27,22 @@ void setup() {
   // writeReg(I2C0_BASE | 0x7c, 16, 16, 40);
 
   writeReg(I2C_En, 1, 0, 0x1); // Enable I2C
+  delay(20000);
 }
 
 void loop() {
- 
-  byte error, address;
-  int nDevices;
- 
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address <= 127; address++)
-  {
-    // The i2c_scanner uses the return value of
-    // Wire.endTransmission to see if
-    // a device acknowledged the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
- 
-  delay(5000);           // wait 5 seconds for next scan
+  Serial.print("Read val ");
+  int out = readI2CInt(1);
+  Serial.print(out);
+  Serial.print("\t Write 452\t");
+  writeI2CInt(4548, 1);
+  
+  out = readI2CInt(1);
+  Serial.print("written ");
+  Serial.println(out);
+  delay(5000);
+  writeI2CInt(0, 1);
+  Serial.println("Cleared");
  
 }
 //IO_BANK0_BASE | 0x004)
@@ -75,9 +51,25 @@ void writeReg(uint32_t addr, uint8_t numbBits, uint8_t offset, uint32_t data) {
   io_rw_32* addrPnt = (io_rw_32*)(addr);
   long clrMask = numbBits == 32 ? 0xFFFFFFFF : (0x1 << (numbBits)) - 1;
   hw_clear_bits(addrPnt, clrMask << offset);
-  hw_set_bits(addrPnt, data << offset);
+  hw_set_bits(addrPnt, (data & clrMask) << offset);
+}
+void writeI2CInt(int data, byte reg) {
+  Wire.beginTransmission(0x13);
+  Wire.write(reg << 1);
+  Wire.write((byte)(data & 0x00FF));
+  Wire.write((byte)(data >> 8));
+  Wire.endTransmission();
 }
 
+int readI2CInt(byte reg) {
+  Wire.beginTransmission(0x13);
+  Wire.write((reg << 1) | 0x1);
+  Wire.endTransmission();
+  Wire.requestFrom(0x13, 2);
+  int output = Wire.read();
+  output |= (Wire.read() << 8);
+  return output;
+}
 // Init
 // hw_set_bits(TEST, 0x3 << 12); // Set bits enable output
 
--- I2C_Secondary/src/main.cpp
+++ I2C_Secondary/src/main.cpp
@@ -1,11 +1,43 @@
 #include <Arduino.h>
 #include <Wire.h>
+//LSB first
+int* arrMap[30];
+int testVal = 0;
+byte globAddr = 0;
 void writeReg(uint32_t addr, uint8_t numbBits, uint8_t offset, uint32_t data);
 uint32_t I2C_Con = I2C0_BASE | 0x00;
 uint32_t I2C_En = I2C0_BASE | 0x6c;
+void receiveEvent(int howMany)
+{
+  globAddr = Wire.read();
+  bool isRead = globAddr & 0x1 ? true : false;
+  globAddr = globAddr >> 1;
+  if(isRead) return;
+  else {
+    byte tempBuffer[2];
+    if(Wire.available() >= 2) {
+      for(int i = 0; i < 2; i++) {
+        tempBuffer[i] = Wire.read();
+      }
+    }
+    int* valSave = arrMap[globAddr];
+    int value = tempBuffer[0] + (tempBuffer[1] << 8);
+    *valSave = value;
+  }
+}
+void requestEvent() {
+  int* valRead = arrMap[globAddr];
+  int value = *valRead;
+  Wire.write((byte)(*valRead & 0x00FF)); // LSB first
+  Wire.write((byte)(*valRead >> 8)); // MSB second
+}
 void setup() {
+  arrMap[1] = &testVal;
   // put your setup code here, to run once:
+  Serial.begin(115200);
   Wire.begin(0x13);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
   writeReg(I2C_En, 1, 0, 0x0);
   writeReg(I2C_Con, 2, 1, 0x2); // Turn on fast mode
   writeReg(I2C0_BASE | 0x94, 8, 0, 12); // SDA Enable (BOTH)
@@ -16,11 +48,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(100);
 }
 
 void writeReg(uint32_t addr, uint8_t numbBits, uint8_t offset, uint32_t data) {
   io_rw_32* addrPnt = (io_rw_32*)(addr);
   long clrMask = numbBits == 32 ? 0xFFFFFFFF : (0x1 << (numbBits)) - 1;
   hw_clear_bits(addrPnt, clrMask << offset);
-  hw_set_bits(addrPnt, data << offset);
+  hw_set_bits(addrPnt, (data & clrMask) << offset);
 }
